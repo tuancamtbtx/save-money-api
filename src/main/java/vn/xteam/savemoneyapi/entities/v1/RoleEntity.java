50244104465b5301ac127eb1b06e00f6e4ad73ef
tuan.nguyen15 <nguyenvantuan140397@gmail.com>
2021-05-10 09:50:05 +0700

    [add] rbac role entity

---
+++ src/main/java/vn/xteam/savemoneyapi/entities/v1/RoleEntity.java
@@ -0,0 +1,13 @@
+package vn.xteam.savemoneyapi.entities.v1;
+
+import java.sql.Timestamp;
+
+public class RoleEntity {
+    private String id;
+    private String name;
+    private String slug;
+    private int active;
+    private String description;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+}
---
+++ src/main/java/vn/xteam/savemoneyapi/entities/v1/RolePermission.java
@@ -0,0 +1,10 @@
+package vn.xteam.savemoneyapi.entities.v1;
+
+import java.sql.Timestamp;
+
+public class RolePermission {
+    private String roleId;
+    private String permissionId;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+}
