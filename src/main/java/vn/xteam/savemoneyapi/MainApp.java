f0ad0b8a3728a1890c06013c28eb2b889dfc0081
tuan.nguyen15 <nguyenvantuan140397@gmail.com>
2021-05-11 22:36:51 +0700

    [add] config datasource

--- src/main/java/vn/xteam/savemoneyapi/MainApp.java
+++ src/main/java/vn/xteam/savemoneyapi/MainApp.java
@@ -2,8 +2,9 @@ package vn.xteam.savemoneyapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MainApp {
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class, args);
